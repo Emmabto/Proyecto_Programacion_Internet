4a64f0fc884549da257be30867359a8f080f172f
Martín Emmanuel Bastida Toledo <bastidaemmanuel12@gmail.com>
2024-10-02 20:28:31 -0600

    CRUD Mascotas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\MascotaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('mascota', MascotaController::class)->parameters(['mascota' => 'mascota']);
